e7cbbb65427d10f9b45c167cc8a9d0c4d1488ad1
Peyam M <maroufi@kth.se>
2018-11-13 13:37:41 +0100

    Lesson 2 Pointers and variables

---
+++ Lesson 2/main.c
@@ -0,0 +1,15 @@
+
+int main() {
+    unsigned int counter = 0;
+    
+    while(counter < 21){
+        ++counter;
+        if((counter & 1) != 0){
+            /* do something */
+        }else{
+          
+        }
+    }
+    
+    return 0;
+}
