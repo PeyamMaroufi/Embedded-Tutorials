714d255ad56e6b6d5de3c4533a40a49c2d9366b3
Peyam M <maroufi@kth.se>
2018-11-13 16:07:22 +0100

    ändrat
    fixat

--- Lesson 2/main.c
+++ Lesson 2/main.c
@@ -1,14 +1,9 @@
-
+unsigned int counter = 0;
 int main() {
-    unsigned int counter = 0;
+    
     
     while(counter < 21){
         ++counter;
-        if((counter & 1) != 0){
-            /* do something */
-        }else{
-          
-        }
     }
     
     return 0;
